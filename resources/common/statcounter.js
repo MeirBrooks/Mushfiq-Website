e8f51240a769042bb7a45c19521e57013796ff5a
Derek Wolfson <derekwolfson@gmail.com>
2015-05-20 16:41:35 -0400

    Added CSS & link to statcounter.js
    1 - Added CSS for Heading/Subheading/Bodytext.  Can now safely use these
    to keep the format of the webpage consistent across pages.
    
    2 - Added reference to statcounter.js (see bottom of pages) instead of
    including the javascript code in each file.  This is mostly just incase
    we need to change the statcounter.js to accommodate changes.  Find
    statcounter.js in resources/common

---
+++ resources/common/statcounter.js
@@ -0,0 +1,8 @@
+var sc_project=10010630; 
+var sc_invisible=1; 
+var sc_security="e7f9a875"; 
+var scJsHost = (("https:" == document.location.protocol) ? 
+"https://secure." : "http://www."); 
+document.write("<sc"+"ript type='text/javascript' src='" + 
+scJsHost+ 
+"statcounter.com/counter/counter.js'></"+"script>");
